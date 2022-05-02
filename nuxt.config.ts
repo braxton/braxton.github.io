5ba71de7e8699eaf8a0e1f9f46a6d72aba159c51
Alicja <25778864+braxton@users.noreply.github.com>
2022-05-02 14:04:47 -0400

    idk at this point

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
       postcssOptions: require("./postcss.config.js"),
     },
     transpile: [
+      "@/assets/css/styles.css",
       "@fortawesome/fontawesome-svg-core",
       "@fortawesome/free-brands-svg-icons",
       "@fortawesome/free-solid-svg-icons",
@@ -16,5 +17,6 @@ export default defineNuxtConfig({
   css: [
     "@/assets/css/styles.css",
     "@fortawesome/fontawesome-svg-core/styles.css"
-  ]
+  ],
+  target: "static"
 })
